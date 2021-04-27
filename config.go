a3893254d89e2acde14d673d907c09109f266018
Jules Michael <michaelj@ringier.co.za>
2021-04-27 19:01:42 +0400

    typo

--- config.go
+++ config.go
@@ -4,8 +4,8 @@ type config struct {
 	AWSAccessKeyID     string `envconfig:"AWS_ACCESS_KEY_ID" required:"true"`
 	AWSSecretAccessKey string `envconfig:"AWS_SECRET_ACCESS_KEY" required:"true"`
 	AWSRegion          string `envconfig:"AWS_REGION"`
-	GithubAuthToken    string `envconfig:"GITHUBORGNAME" required:"true"`
-	GithubOrgName      string `envconfig:"GITHUBOAUTHTOKEN" required:"true"`
+	GithubOrgName      string `envconfig:"GITHUBORGNAME" required:"true"`
+	GithubAuthToken    string `envconfig:"GITHUBOAUTHTOKEN" required:"true"`
 	DNSDomain          string `envconfig:"DNSDOMAIN"`
 	DNSZoneID          string `envconfig:"DNSZONEID"`
 	BackendURL         string `envconfig:"BACKEND_URL" required:"true"`
