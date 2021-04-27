2bd6c5570d780c6bd32dd6209804dbf74320ac27
Jules Michael <michaelj@ringier.co.za>
2021-04-27 12:36:13 +0400

    add bastion ami id

--- environment.go
+++ environment.go
@@ -17,6 +17,7 @@ type environment struct {
 	GithubAuthToken   string `json:"github_auth_token"`
 	GithubOrgName     string `json:"github_org_name"`
 	ApiGatewayBaseDef string `json:"api_gateway_base_definition"`
+	BastionAMIID      string `json:"bastion_ami_id"`
 }
 
 func (e environment) Validate() error {
@@ -62,5 +63,9 @@ func (e *environment) SetDefaults(cfg config) {
 
 	if e.ApiGatewayBaseDef == "" {
 		e.ApiGatewayBaseDef = `{"swagger":"2.0","info":{"version":"1.0.0","title":"API TITLE"},"schemes":["https"],"paths":{"/events":{"post":{"produces":["application/json"],"responses":{"200":{"description":"200 response"},"500":{"description":"500 response"}},"x-amazon-apigateway-integration":{"uri":"https://www.zebroc.de/h00k/","responses":{"default":{"statusCode":"200"}},"passthroughBehavior":"when_no_match","httpMethod":"POST","type":"http"}}},"/login":{"post":{"responses":{"200":{"description":"200 response"},"400":{"description":"400 response"},"500":{"description":"500 response"},"401":{"description":"401 response"},"404":{"description":"404 response"}},"x-amazon-apigateway-integration":{"uri":"https://www.zebroc.de/h00k/","responses":{"200":{"statusCode":"200"},"400":{"statusCode":"400"},"500":{"statusCode":"500"},"401":{"statusCode":"401"},"404":{"statusCode":"404"}},"passthroughBehavior":"when_no_match","httpMethod":"POST","type":"http"}}}},"securityDefinitions":{"api_key":{"type":"apiKey","name":"x-api-key","in":"header"}}}`
+	}
+
+	if e.BastionAMIID == "" {
+		e.BastionAMIID = "ami-0aef57767f5404a3c"
 	}
 }
